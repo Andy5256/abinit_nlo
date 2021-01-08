97acd511d1dc6b86b469d0980d5e1aedd3647377
Andy5256 <wangjizhang1990@126.com>
2021-01-08 19:17:02 +0800

    plot static shg w.r.t. nkpt and nbands

---
+++ abinit-band/data.py
@@ -0,0 +1,9 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+# with open('data.dat') as f:
+#     info = f.readline
+info = np.loadtxt('k11.dat')
+plt.plot(info[:,0], info[:,1], '-o')
+# %%
--- abinit-band/plot_band.py
+++ abinit-band/plot_band.py
@@ -22,7 +22,7 @@ E_k = np.array(E_k).T - 0.41633 * 27.2113845 *0
 for Ei in E_k:
     # plt.plot(Ei + 0.14101 * 27.211 * 0.7, c='r') # graphene
     plt.plot(Ei - 0.14287 * 27.211, c='b') # AgGaS2
-plt.ylim(-6,6)
+# plt.ylim(-6,6)
 plt.xlim(0,nkpts-1)
 plt.hlines(0,xmin = 0, xmax = nkpts - 1, colors = 'k', linestyles='--')
 
@@ -40,14 +40,13 @@ xtick_list.append(nkpts)
 # kpath = [chr(915), 'K', 'M', chr(915)] # kpath for graphene
 kpath = [chr(915), 'X', 'Y', chr(931), chr(915), 'Z', chr(931) + '_1', 'N', 'P', 'Y_1', 'Z', 'X', 'P'] # kpath for AgGaS2
 for i in xtick_list:
-    plt.vlines(i, -10, 10, colors='k', )
-    plt.vlines(i, -10, 10, )
+    plt.vlines(i, -10, 10, colors='k')
 plt.xticks(xtick_list, kpath)
 matplotlib.rcParams['xtick.direction'] = 'in'
 matplotlib.rcParams['ytick.direction'] = 'in'
 # plt.show()
 # plt.savefig('graphene_goodkpath.png')
-plt.savefig('Band_AgGaS2.png')
+# plt.savefig('Band_AgGaS2.png')
 
 
 # %%
