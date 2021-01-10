255e765be1ce203f7323398b7bb92525f648559d
Andy5256 <wangjizhang1990@126.com>
2021-01-10 21:30:14 +0800

    plot band resolved static shg

--- WSHG/plot.py
+++ WSHG/plot.py
@@ -2,9 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-info = np.loadtxt('ReSHG_312.dat')
+info = np.loadtxt('ReSHG_123.dat')
 
-plt.plot(info[0,:])
-plt.xlim(25,75)
+plt.plot(info[0,:], 'o-')
+plt.xlim(0,75)
+plt.xlabel('Band Index')
+plt.ylabel('$χ_{ij}^{(2)}$(pm/V)')
+plt.savefig('AgGaS2_ReSHG_123_bandidx_shg.png',dpi=500)
 
 # %%
